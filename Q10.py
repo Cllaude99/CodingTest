c9bf6055349941f9d89f32d365c0e671547eb4c2
KimTaeYoon <xodbsrla1025@naver.com>
2023-02-26 14:29:54 +0900

    Update

--- Q10.py
+++ Q10.py
@@ -1,16 +1,13 @@
 # 최솟값 찾기1
-import sys
-input = sys.stdin.readline
+from collections import deque
 N, L = map(int, input().split())
-numList = list(map(int, input().split()))
-Result = []
+mydeque = deque()
+now = list(map(int, input().split()))
 
 for i in range(N):
-    start_index = 0 if i-L+1 < 0 else i-L+1
-    end_index = i
-    arr = []
-    for j in range(start_index, end_index + 1):
-        arr.append(numList[j])
-    Result.append(min(arr))
-
-print(Result)
+    while mydeque and mydeque[-1][0] > now[i]:
+        mydeque.pop()
+    mydeque.append((now[i], i))
+    if mydeque[0][1] <= i - L:
+        mydeque.popleft()
+    print(mydeque[0][0], end=' ')
