8e6b41d350196cf43c47da4118d98dea5f23c7f3
TaeYoon Kim <xodbsrla1025@naver.com>
2023-06-21 17:24:11 +0900

    Refactor: "question1 refactoring"

--- Q1.py
+++ Q1.py
@@ -1,9 +1,13 @@
 # 숫자의 합 구하기
-n = input()
-numbers = list(input())
+
+N = int(input('')) #숫자의 개수
+numList = input('') #공백 없이 주어진 N개의 숫자
+
 sum = 0
 
-for i in numbers:
-    sum += int(i)
+for i in range(len(numList)):
+    sum += int(numList[i])
 
 print(sum)
+    
+
