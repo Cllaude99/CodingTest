4d1586ffd11514d8c24d132009068110accccafa
TaeYoon Kim <xodbsrla1025@naver.com>
2023-06-21 20:22:42 +0900

    Refator: "question3 refactoring"

--- Q3.py
+++ Q3.py
@@ -1,15 +1,15 @@
-# 구간합
+# 구간 합 구하기1
+
 import sys
 input = sys.stdin.readline
-suNo, quizNo = map(int, input().split())
-numbers = list(map(int, input().split()))
-prefix_sum = [0]
-temp = 0
 
-for i in numbers:
-    temp = temp + i
-    prefix_sum.append(temp)
+N, M = map(int, input().split(' '))
+arr = list(map(int, input().split(' ')))
+arrSum = [0]*(N+1)
 
-for i in range(quizNo):
-    s, e = map(int, input().split())
-    print(prefix_sum[e] - prefix_sum[s - 1])
+for i in range(1, N + 1):
+    arrSum[i] = arrSum[i-1] + arr[i - 1]
+
+for _ in range(M):
+    start, end = map(int, input().split(' '))
+    print(arrSum[end] - arrSum[start - 1])
