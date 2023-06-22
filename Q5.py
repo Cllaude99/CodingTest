6373887d31f17cd55bf6e4cf5ba3e29549654e73
TaeYoon Kim <xodbsrla1025@naver.com>
2023-06-22 11:54:36 +0900

    Refactor: "question5 refactoring"

--- Q5.py
+++ Q5.py
@@ -1,24 +1,28 @@
-# 나머지 합 구하기
+# 나머지 합
+
 import sys
 input = sys.stdin.readline
-n, m = map(int, input().split())
-A = list(map(int, input().split()))
-S = [0] * n
-C = [0] * m
-S[0] = A[0]
-answer = 0
 
-for i in range(1, n):
-    S[i] = S[i - 1] + A[i]
+N, M = map(int, input().split())
+numList = list(map(int, input().split()))
 
-for i in range(n):
-    remainder = S[i] % m
-    if remainder == 0:
-        answer += 1
-    C[remainder] += 1
+arr = [0]*(N+1)  # 입력 받은 배열
+sumArr = [0]*(N+1)  # 구간 합 배열
+left = [0]*M  # 나머지 값이 동일한 값들의 개수 (인덱스는 나머지 값을 의미함)
+count = 0  # 정답 개수
 
-for i in range(m):
-    if C[i] > 1:
-        answer += (C[i] * (C[i] - 1) // 2)
+for i in range(1, N+1):
+    arr[i] = numList[i-1]
+    sumArr[i] = sumArr[i-1] + arr[i]
 
-print(answer)
+for i in range(1, N+1):
+    value = sumArr[i] % M
+    if value == 0:
+        count += 1
+    left[value] += 1
+
+for i in range(M):
+    if left[i] > 1:
+        count += (left[i] * (left[i]-1)) // 2
+
+print(count)
