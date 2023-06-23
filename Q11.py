72200e92296decd2f88675c94c8c7b2e136f549f
TaeYoon Kim <xodbsrla1025@naver.com>
2023-06-23 16:16:01 +0900

    Refactor: "question11 refactoring"

--- Q11.py
+++ Q11.py
@@ -1,31 +1,39 @@
 # 스택으로 수열 만들기
+
+import sys
+input = sys.stdin.readline
+
 N = int(input())
-A = [0]*N
+goal = [None]*N
+Result = ""
+lastNum = 1  # 어디까지 스택에 추가해주었는지 기억하기 위한 변수
+check = True
+stk = []
 
 for i in range(N):
-    A[i] = int(input())
+    goal[i] = int(input())
 
-stack = []
-num = 1
-result = True
-answer = ""
-
-for i in range(N):
-    su = A[i]
-    if su >= num:
-        while su >= num:
-            stack.append(num)
-            num += 1
-            answer += "+\n"
-        stack.pop()
-        answer += "-\n"
+for i in range(N):  # 스택의 peek 값과 주여진 수열의 값들을 비교해가는 과정
+    if len(stk) == 0:
+        while lastNum <= goal[i]:
+            stk.append(lastNum)
+            lastNum += 1
+            Result += "+"
+    if stk[-1] == goal[i]:
+        stk.pop()
+        Result += '-'
+    elif stk[-1] < goal[i]:
+        while lastNum <= goal[i]:
+            stk.append(lastNum)
+            lastNum += 1
+            Result += "+"
+        stk.pop()
+        Result += '-'
     else:
-        n = stack.pop()
-        if n > su: 
-          print("NO")
-          result = False
-          break
-        else:
-            answer += "-\n"
-if result:
-    print(answer)
+        print("NO")
+        check = False
+        break
+
+if check:
+    for i in range(len(Result)):
+        print(Result[i])
