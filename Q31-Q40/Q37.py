81b48dcaf38aae1c50b5bb0f52e96f6814039371
TaeYoon Kim <xodbsrla1025@naver.com>
2023-07-14 22:27:31 +0900

    Refactor:"question37 refactoring"

---
+++ Q31-Q40/Q37.py
@@ -0,0 +1,17 @@
+# 소수 구하기
+import math
+M, N = map(int, input().split())
+arr = [0] * (N + 1)
+
+for i in range(2, N + 1):
+    arr[i] = i
+
+for i in range(2, int(math.sqrt(N)) + 1):
+    if arr[i] == 0:
+        continue
+    for j in range(i+i, N + 1, i):
+        arr[j] = 0
+
+for i in range(M, N + 1):
+    if arr[i] != 0:
+        print(arr[i])
