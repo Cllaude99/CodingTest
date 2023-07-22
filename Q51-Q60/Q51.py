49b466abc77e49576900f25dd8cc1c3a8eab0e05
TaeYoon Kim <xodbsrla1025@naver.com>
2023-07-22 23:11:42 +0900

    Backjoon:"1976번 여행가자"

--- Q51-Q60/Q51.py
+++ Q51-Q60/Q51.py
@@ -6,3 +6,33 @@ input = sys.stdin.readline
 
 N = int(input())
 M = int(input())
+arr = [0] * (N + 1)
+success = True
+
+for i in range(N + 1):
+    arr[i] = i
+
+def find(idx):
+    if idx == arr[idx]:
+        return idx
+    else:
+        arr[idx] = find(arr[idx])
+        return arr[idx]
+
+for i in range(N):
+    routes = list(map(int, input().split()))
+    for r in range(len(routes)):
+        if routes[r] == 1:
+            arr[find(r + 1)] = arr[find(i + 1)]
+
+schedule = list(map(int, input().split()))
+before = schedule[0]
+for route in schedule[1:]:
+    if find(before) != find(route):
+        success = False
+        break
+    before = route
+
+if success:
+    print("YES")
+else: print("NO")
