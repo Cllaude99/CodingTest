24a9489f5a2431efa58c15e3557fc07e01b3911a
TaeYoon Kim <xodbsrla1025@naver.com>
2023-08-11 23:18:33 +0900

    Backjoon:"1414번 불우이웃돕기"

---
+++ Q61-Q70/Q67.py
@@ -0,0 +1,30 @@
+# 트리의 부모 찾기
+
+import sys
+sys.setrecursionlimit(10 ** 6)
+input = sys.stdin.readline
+
+N = int(input())
+arr = [[] for _ in range(N + 1)]
+result = [0] * (N + 1)
+visited = [False] * (N + 1)
+
+for _ in range(N - 1):
+    f, s = map(int, input().split())
+    arr[f].append(s)
+    arr[s].append(f)
+
+
+def DFS(idx, p):
+    visited[idx] = True
+    result[idx] = p
+
+    for v in arr[idx]:
+        if not visited[v]:
+            DFS(v, idx)
+
+
+DFS(1, 1)
+
+for v in range(2, N + 1):
+    print(result[v])
