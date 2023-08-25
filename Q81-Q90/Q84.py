c404b778c8b32facc682851c8920194ee15e8e4f
Cllaude99 <xodbsrla1025@naver.com>
2023-08-25 23:29:03 +0900

    Backjoon:"1463번 정수를 1로 만들기"

--- Q81-Q90/Q84.py
+++ Q81-Q90/Q84.py
@@ -1,15 +1,16 @@
 # 1로 만들기
 import sys
 N = int(input())
-DP = [0] * (1000001)
+DP = [sys.maxsize] * (1000001)
 
 DP[1] = 0
 
 for i in range(2, 1000001):
-    DP[i] = 1 + DP[i-1]
+    DP[i] = min(1 + DP[i - 1], DP[i])
     if i % 3 == 0:
-        DP[i] = min(1 + DP[i//3], DP[i])
+        DP[i] = min(1 + DP[i//3], 1 + DP[i-1])
     if i % 2 == 0:
-        DP[i] = min(1 + DP[i//2], DP[i])
+        DP[i] = min(1 + DP[i//2], 1 + DP[i-1])
+
 
 print(DP[N])
