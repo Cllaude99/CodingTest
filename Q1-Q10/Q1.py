358cf52776d72c95c50b2f867a2857307a969af9
Cllaude99 <xodbsrla1025@naver.com>
2023-10-02 17:37:33 +0900

    Refacotor: "Backjoon 11720"

--- Q1-Q10/Q1.py
+++ Q1-Q10/Q1.py
@@ -1,13 +1,6 @@
 # 숫자의 합 구하기
 
-N = int(input('')) #숫자의 개수
-numList = input('') #공백 없이 주어진 N개의 숫자
+N = int(input())
+num = list(map(int, input()))
 
-sum = 0
-
-for i in range(len(numList)):
-    sum += int(numList[i])
-
-print(sum)
-    
-
+print(sum(num))
