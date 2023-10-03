59396c7ec8def3329f65ccbd2fdd455aadb44b3c
Cllaude99 <xodbsrla1025@naver.com>
2023-10-03 16:21:35 +0900

    Refactor:"Backjoon 11660"

--- Q1-Q10/Q4.py
+++ Q1-Q10/Q4.py
@@ -1,22 +1,21 @@
-# 구간 합 구하기5
+# 구간합 구하기5
 
 import sys
 input = sys.stdin.readline
 
 N, M = map(int, input().split())
-arr = [[0]*(N+1)]
-sumArr = [[0 for j in range(N+1)] for i in range(N+1)]
+arr = [[0 for j in range(N + 1)] for i in range(N + 1)]
+sumArr = [[0 for j in range(N + 1)] for i in range(N + 1)]
 
-for i in range(N):
-    numList = [0] + [int(x) for x in input().split()]
-    arr.append(numList)
+for i in range(1, N + 1):
+    numList = list(map(int, input().split()))
+    for j in range(1, len(numList) + 1):
+        arr[i][j] = numList[j-1]
+        sumArr[i][j] = sumArr[i-1][j] + \
+            sumArr[i][j-1] - sumArr[i-1][j-1] + arr[i][j]
 
-for i in range(1, N+1):
-    for j in range(1, N+1):
-        sumArr[i][j] = sumArr[i][j-1] + \
-            sumArr[i-1][j] - sumArr[i-1][j-1] + arr[i][j]
-
-for _ in range(M):
-    X1, Y1, X2, Y2 = map(int, input().split())
-    print(sumArr[X2][Y2] - sumArr[X1-1][Y2] -
-          sumArr[X2][Y1-1] + sumArr[X1-1][Y1-1])
+for i in range(M):
+    x1, y1, x2, y2 = map(int, input().split())
+    result = sumArr[x2][y2] - \
+        (sumArr[x2][y1-1] + sumArr[x1-1][y2]) + sumArr[x1-1][y1-1]
+    print(result)
