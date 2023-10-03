e4f2f26e7c29100061712f3435e7a30988c8acb5
Cllaude99 <xodbsrla1025@naver.com>
2023-10-03 11:01:49 +0900

    Refactor:"Backjoon 11659"

--- Q1-Q10/Q3.py
+++ Q1-Q10/Q3.py
@@ -1,15 +1,16 @@
-# 구간 합 구하기1
+# 구간 합 구하기
 
 import sys
 input = sys.stdin.readline
 
-N, M = map(int, input().split(' '))
-arr = list(map(int, input().split(' ')))
-arrSum = [0]*(N+1)
+N, M = map(int, input().split())
+arr = list(map(int, input().split()))
+sumArr = [0] * (N + 1)
 
+sumArr[0] = 0
 for i in range(1, N + 1):
-    arrSum[i] = arrSum[i-1] + arr[i - 1]
+    sumArr[i] = sumArr[i-1] + arr[i - 1]
 
-for _ in range(M):
-    start, end = map(int, input().split(' '))
-    print(arrSum[end] - arrSum[start - 1])
+for i in range(M):
+    f, s = map(int, input().split())
+    print(sumArr[s] - sumArr[f-1])
