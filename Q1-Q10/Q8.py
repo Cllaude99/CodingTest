f4354fb1d2c222d8bb774537a1a8816a36431887
Cllaude99 <xodbsrla1025@naver.com>
2023-10-10 22:21:09 +0900

    Refactor:"Backjoon 1253"

--- Q1-Q10/Q8.py
+++ Q1-Q10/Q8.py
@@ -1,23 +1,23 @@
 # 좋은 수 구하기
 
 N = int(input())
-numList = list(map(int, input().split()))
-numList.sort()
+arr = list(map(int, input().split()))
+arr.sort()
 count = 0
 
 for i in range(N):
     start = 0
     end = N - 1
     while start < end:
-        if numList[start] + numList[end] == numList[i]:
+        if arr[start] + arr[end] == arr[i]:
             if start != i and end != i:
                 count += 1
                 break
-            elif start == i:
-                start += 1
-            else:
+            elif end == i:
                 end -= 1
-        elif numList[start] + numList[end] < numList[i]:
+            else:
+                start += 1
+        elif arr[start] + arr[end] < arr[i]:
             start += 1
         else:
             end -= 1
