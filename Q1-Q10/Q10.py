d3305e9dead8d21e0dd3e04fbe8f783d14815e22
TaeYoon Kim <xodbsrla1025@naver.com>
2023-10-12 12:29:08 +0900

    Refactor:"Backjoon 11003"

--- Q1-Q10/Q10.py
+++ Q1-Q10/Q10.py
@@ -1,17 +1,19 @@
 # 최솟값 찾기
+
 from collections import deque
 import sys
 input = sys.stdin.readline
 
-N, L = map(int, input().split())
-numList = list(map(int, input().split()))
+N,L = map(int, input().split())
+D = list(map(int, input().split()))
 deq = deque()
 
 for i in range(N):
-    while len(deq) != 0 and deq[-1][1] > numList[i]:
-        deq.pop()
-    deq.append((i+1, numList[i]))
+  while len(deq) > 0 and deq[-1][0] > D[i]:
+    deq.pop()    
+  deq.append((D[i], i))
 
-    while (i+1) - deq[0][0] >= L:
-        deq.popleft()
-    print(deq[0][1], end=' ')
+  while i - deq[0][1] >= L:
+    deq.popleft()
+
+  print(deq[0][0], end=' ')
