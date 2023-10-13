168f63a27b367f43bb7c8ed3cef066c7fc02f68d
Cllaude99 <xodbsrla1025@naver.com>
2023-10-13 16:41:23 +0900

    Refactor:"Backjoon 1874"

--- Q11-Q20/Q11.py
+++ Q11-Q20/Q11.py
@@ -4,36 +4,34 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-goal = [None]*N
-Result = ""
-lastNum = 1  # 어디까지 스택에 추가해주었는지 기억하기 위한 변수
-check = True
+sequence = []
 stk = []
-
+result = []
 for i in range(N):
-    goal[i] = int(input())
+    sequence.append(int(input()))
+sequencePtr = 0
+currentNumber = 1
+success = True
 
-for i in range(N):  # 스택의 peek 값과 주여진 수열의 값들을 비교해가는 과정
-    if len(stk) == 0:
-        while lastNum <= goal[i]:
-            stk.append(lastNum)
-            lastNum += 1
-            Result += "+"
-    if stk[-1] == goal[i]:
+while sequencePtr < N:
+    if not stk or stk[-1] < sequence[sequencePtr]:
+        while currentNumber <= sequence[sequencePtr]:
+            stk.append(currentNumber)
+            currentNumber += 1
+            result.append('+')
         stk.pop()
-        Result += '-'
-    elif stk[-1] < goal[i]:
-        while lastNum <= goal[i]:
-            stk.append(lastNum)
-            lastNum += 1
-            Result += "+"
-        stk.pop()
-        Result += '-'
-    else:
-        print("NO")
-        check = False
+        result.append('-')
+        sequencePtr += 1
+    elif sequence[sequencePtr] < stk[-1]:
+        success = False
         break
+    elif sequence[sequencePtr] == stk[-1]:
+        stk.pop()
+        result.append('-')
+        sequencePtr += 1
 
-if check:
-    for i in range(len(Result)):
-        print(Result[i])
+if success:
+    for v in result:
+        print(v)
+else:
+    print("NO")
