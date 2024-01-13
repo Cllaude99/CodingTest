9429d4a5185b3048b50ede92cda35311c03794f9
TaeYoon Kim <xodbsrla1025@naver.com>
2024-01-13 11:16:04 +0900

    [Silver IV] Title: 수 찾기, Time: 344 ms, Memory: 42104 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -9,29 +9,24 @@ const arr = input.shift().split(' ').map(Number);
 const M = Number(input.shift());
 const temp = input.shift().split(' ').map(Number);
 let answer = [];
-
 arr.sort((a, b) => a - b);
 
 temp.forEach((element) => {
   let pl = 0;
   let pr = arr.length - 1;
-  let find = false;
+
   while (pl <= pr) {
     let mid = Math.floor((pl + pr) / 2);
     if (arr[mid] === element) {
-      find = true;
-      break;
+      answer.push(1);
+      return;
     } else if (arr[mid] < element) {
       pl = mid + 1;
     } else {
       pr = mid - 1;
     }
   }
-  if (find) {
-    answer.push(1);
-  } else {
-    answer.push(0);
-  }
+  answer.push(0);
 });
 
-console.log(answer.join('\n'));
+console.log(answer.join("\n"))
