065ffa363cc54cf4d1be70f1eb2beb4618e6c292
TaeYoon Kim <xodbsrla1025@naver.com>
2024-01-15 23:42:09 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 256 ms, Memory: 21080 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.js
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.js
@@ -1,80 +1,76 @@
+const input = require('fs')
+  .readFileSync('dev/stdin')
+  .toString()
+  .trim()
+  .split('\n');
+
+const N = Number(input.shift());
+const arr = [];
+for (let i = 0; i < N; i++) {
+  arr.push(Number(input[i]));
+}
+let compareCnt = 0;
+
 class MinHeap {
   constructor() {
     this.heap = [];
   }
-
-  getLength = () => {
+  getSize() {
     return this.heap.length;
-  };
+  }
+  swap(idx1, idx2) {
+    let temp = this.heap[idx1];
+    this.heap[idx1] = this.heap[idx2];
+    this.heap[idx2] = temp;
+  }
+  add(item) {
+    this.heap.push(item);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let currentIdx = this.heap.length - 1;
+    let parentIdx = Math.floor((currentIdx - 1) / 2);
 
-  push = (node) => {
-    this.heap.push(node);
-    let i = this.heap.length - 1;
-    let parentI = Math.floor((i - 1) / 2);
-    while (i > 0 && this.heap[parentI] > this.heap[i]) {
-      this.swap(i, parentI);
-      i = parentI;
-      parentI = Math.floor((i - 1) / 2);
+    while (parentIdx >= 0 && this.heap[currentIdx] < this.heap[parentIdx]) {
+      this.swap(currentIdx, parentIdx);
+      currentIdx = parentIdx;
+      parentIdx = Math.floor((currentIdx - 1) / 2);
     }
-  };
-
-  pop = () => {
-    if (this.heap.length === 1) {
+  }
+  getMinValue() {
+    if (this.getSize() === 1) {
       return this.heap.pop();
     }
-
-    const result = this.heap[0];
+    const value = this.heap[0];
     this.heap[0] = this.heap.pop();
-    let i = 0;
-    while (true) {
-      const leftI = i * 2 + 1,
-        rightI = i * 2 + 2;
-      if (leftI >= this.heap.size) {
-        break;
-      }
-      let nextI = i;
-      if (this.heap[nextI] > this.heap[leftI]) {
-        nextI = leftI;
-      }
-      if (rightI < this.heap.length && this.heap[nextI] > this.heap[rightI]) {
-        nextI = rightI;
-      }
-      if (nextI === i) {
-        break;
-      }
-      this.swap(i, nextI);
-      i = nextI;
-    }
-    return result;
-  };
+    this.bubbleDown();
+    return value;
+  }
+  bubbleDown() {
+    let parentIdx = 0;
 
-  swap = (a, b) => {
-    const temp = this.heap[a];
-    this.heap[a] = this.heap[b];
-    this.heap[b] = temp;
-  };
+    while (parentIdx < Math.floor(this.getSize() / 2)) {
+      let pl = 2 * parentIdx + 1;
+      let pr = pl + 1;
+      let child =
+        pr <= this.getSize() - 1 && this.heap[pr] < this.heap[pl] ? pr : pl;
+      if (this.heap[parentIdx] < this.heap[child]) break;
+      this.swap(parentIdx, child);
+      parentIdx = child;
+    }
+  }
 }
 
-const input = [];
+const myheap = new MinHeap();
+for (let value of arr) {
+  myheap.add(value);
+}
 
-require('readline')
-  .createInterface(process.stdin, process.stdout)
-  .on('line', function (line) {
-    input.push(line.trim());
-  })
-  .on('close', function () {
-    const minHeap = new MinHeap();
-    for (let i = 1; i < input.length; i++) {
-      minHeap.push(+input[i]);
-    }
+while (myheap.getSize() > 1) {
+  const first = myheap.getMinValue();
+  const second = myheap.getMinValue();
+  compareCnt += first + second;
+  myheap.add(first + second);
+}
 
-    let totalCompareCount = 0;
-    while (minHeap.getLength() > 1) {
-      let aCount = minHeap.pop();
-      let bCount = minHeap.pop();
-      let compareCount = aCount + bCount;
-      totalCompareCount += compareCount;
-      minHeap.push(compareCount);
-    }
-    console.log(totalCompareCount);
-  });
+console.log(compareCnt);
