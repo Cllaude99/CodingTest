544542d4f4bca8c407ebb407a15332586506312b
TaeYoon Kim <xodbsrla1025@naver.com>
2024-08-10 23:18:08 +0900

    [goormlevel] Title: 계수기 만들기, Time: 0.03 ms, Memory: 33.86 MB, Difficulty: 0 -BaekjoonHub

--- goormlevel/43061/1. 계수기 만들기/계수기 만들기.js
+++ goormlevel/43061/1. 계수기 만들기/계수기 만들기.js
@@ -7,63 +7,41 @@ const rl = readline.createInterface({
 });
 
 let N = null;
-let A = null;
-let counter = null;
+let A = [];
+let B = [];
 let K = null;
 
 rl.on("line", function(line) {
-	if(!N){
+	if(N === null){
 		N = +line;
-	}else if(!A){
+	}else if(A.length === 0){
 		A = line.split(" ").map(Number);
-	}else if(!counter){
-		counter = line.split(" ").map(Number);
+	}else if(B.length === 0){
+		B = line.split(" ").map(Number);
 	}else{
 		K = +line;
 		rl.close();
 	}
 }).on("close", function() {
-	console.log(solution(N, A, counter, K));
+	console.log(solution(N, A, B, K));
 	process.exit();
 });
 
-/*
-	문제설계
+function solution(N,A,B,K){
+	let curLocation = N-1;
 	
-	# counter: 계수기
-	# A: i번째 자리에 표시할 수 있는 숫자
-	
-	1. 아래의 과정을 K번 수행한다.
-	
-	2. N-1번째 자리의 값을 1증가한다.
-	3. N-1번째 자리의 값부터 1번째 자리의 값까지 확인해가며, 표시할 수 있는 숫자인지 확인한다.
-	4-1. 해당 자릿수가 표시할 수 범위를 넘어간 경우
-	-  첫번째 자리가 아닌 경우
-	   	해당 자릿수의 값을 0으로 수정한다.
-		 	(해당 자릿수 - 1)번째 값을 1증가한다.
-			3번 과정으로 돌아간다.
-			
-	-  첫번째 자리인 경우
-	   해당 자릿수의 값을 0으로 수정한다.
-		 반복문을 종료한다.
-	4-2. 해당 자릿수가 표시할 수 있는 범위를 넘어가지 않은 경우에는 2번과정으로 돌아간다.
-	
-	5. 최종적으로 계수기에 표시된 숫자를 처음부터 출력한다
-*/
-
-function solution(N, A, counter, K){
-	while(K > 0){
-		counter[N-1]++;
-		
-		for(let i=N-1; i>=0; i--){
-			if(counter[i] <= A[i]) break;
-			
-			counter[i] = 0;
-			if(i !== 0) counter[i-1]++;
-		}
-		
-		K--;
+	for(let i=0; i<K; i++){
+		curLocation = N-1;
+		while(curLocation >= 0){
+				if(B[curLocation] + 1 <= A[curLocation]){
+					B[curLocation]++;
+					break;
+				}else{
+					B[curLocation] = 0;
+					curLocation--;
+				}
+			}
 	}
 	
-	return counter.join("");
+	return B.join('');
 }
